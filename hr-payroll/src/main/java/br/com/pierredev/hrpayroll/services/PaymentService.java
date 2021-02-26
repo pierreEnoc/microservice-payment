1618b178f333d6a57ea3fbc2e486357ad70f12b5
Pierre enoc <pierre.enoc@gympass.com>
2021-02-25 23:45:24 -0300

    feign client

--- hr-payroll/src/main/java/br/com/pierredev/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/pierredev/hrpayroll/services/PaymentService.java
@@ -1,30 +1,21 @@
 package br.com.pierredev.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import br.com.pierredev.hrpayroll.entities.Payment;
 import br.com.pierredev.hrpayroll.entities.Worker;
+import br.com.pierredev.hrpayroll.feignClient.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
-	@Value("${hr-worker.host}")
-	private String workerHost;
-	
 	@Autowired
-	private RestTemplate resteRestTemplate;
+	private WorkerFeignClient workerFeignClient;
 	
 	public Payment getPayment(long workerId, int days) {
-		Map<String, String>  uriVariables = new HashMap<>();
-		uriVariables.put("id", "" + workerId);
-		
-		Worker worker = resteRestTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+				
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 	    return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 
