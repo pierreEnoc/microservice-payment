4921f09ece72c6d32ae167e6b0d1aa3ece6c4624
Pierre enoc <pierre.enoc@gympass.com>
2021-03-02 22:33:40 -0300

    project hr-api-getway-zuul created

---
+++ hr-api-getway-zuul/src/main/java/br/com/pierredev/hrapigetwayzuul/HrApiGetwayZuulApplication.java
@@ -0,0 +1,13 @@
+package br.com.pierredev.hrapigetwayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApiGetwayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGetwayZuulApplication.class, args);
+	}
+
+}
---
+++ hr-api-getway-zuul/src/test/java/br/com/pierredev/hrapigetwayzuul/HrApiGetwayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pierredev.hrapigetwayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGetwayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
