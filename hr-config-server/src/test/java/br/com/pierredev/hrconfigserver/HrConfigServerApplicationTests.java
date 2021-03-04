0f2b69788035eec4c3735e4516f10d118336bcb2
Pierre enoc <pierre.enoc@gympass.com>
2021-03-03 22:58:48 -0300

    hr-config-server created

---
+++ hr-config-server/src/test/java/br/com/pierredev/hrconfigserver/HrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pierredev.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
