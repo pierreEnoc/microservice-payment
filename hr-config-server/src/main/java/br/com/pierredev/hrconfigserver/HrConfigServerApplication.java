6e2947cf31f87cc7343633a8cfe1d86043cbf7c5
Pierre enoc <pierre.enoc@gympass.com>
2021-04-01 11:20:32 -0300

    hr-user postgres dev profile

--- hr-config-server/src/main/java/br/com/pierredev/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/br/com/pierredev/hrconfigserver/HrConfigServerApplication.java
@@ -6,21 +6,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+
 @EnableConfigServer
 @SpringBootApplication
-//public class HrConfigServerApplication implements CommandLineRunner {
-public class HrConfigServerApplication {
-	
-	//@Value("${spring.cloud.config.server.git.username}")
-	//private String username;
+public class HrConfigServerApplication implements CommandLineRunner {
+
+	@Value("${spring.cloud.config.server.git.username}")
+	private String username;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
 	}
-    /*
+
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("USERNAME = " + username);
+		//System.out.println("USERNAME = " + username);
 	}
-  */
+  
 }
