31de6d2cd1f777bcc4cafc92d1acfca9b3c62a7b
Pierre enoc <pierre.enoc@gympass.com>
2021-04-01 16:56:35 -0300

    container hr-config

--- hr-config-server/src/main/java/br/com/pierredev/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/br/com/pierredev/hrconfigserver/HrConfigServerApplication.java
@@ -11,8 +11,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 public class HrConfigServerApplication implements CommandLineRunner {
 
-	@Value("${spring.cloud.config.server.git.username}")
-	private String username;
+	//@Value("${spring.cloud.config.server.git.username}")
+	//private String username;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
